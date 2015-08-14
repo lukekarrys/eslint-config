476bed6a27e3fddddb00973ec6fe30125de4d05d
Luke Karrys <lukekarrys@gmail.com>
2015-08-13 20:28:44 -0700

    alce; comment out some stuff before 1.2.0

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require('./eslintrc.json');
+var alce = require('alce');
+var path = require('path');
+var fs = require('fs');
+
+module.exports = alce.parse(fs.readFileSync(path.join(__dirname, 'eslintrc.json'))); 
