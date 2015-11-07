41677383891e69a84c22833ac79782550248cf30
Luke Karrys <lukekarrys@gmail.com>
2015-11-07 13:26:13 -0700

    use alce fork

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* eslint no-var:0, no-magic-numbers:0 */
+/* eslint no-var:0 */
 
 'use strict';
 
@@ -6,9 +6,4 @@ var alce = require('alce');
 var path = require('path');
 var fs = require('fs');
 
-var config = alce.parse(fs.readFileSync(path.join(__dirname, 'eslintrc.json')));
-
-// alce cant parse the number -1?
-config.rules['no-magic-numbers'][1].ignore = [-1, 0, 1, 2];
-
-module.exports = config;
+module.exports = alce.parse(fs.readFileSync(path.join(__dirname, 'eslintrc.json')));
